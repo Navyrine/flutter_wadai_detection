bdd2df29d051091e9aeed6a7ec751d0790504b29
Navyrine <wahidadinugrohoo@gmail.com>
2025-01-15 22:55:07 +0800

    Memperbarui tampilan gambar pada result screen

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -11,18 +12,51 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Detection Result')),
-      body: Container(
-        height: 462,
-        width: 374,
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          image: DecorationImage(
-            image: FileImage(File(imagePath)),
-            fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Container(
+            height: 462,
+            width: 374,
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.only(bottom: 25),
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image: FileImage(File(imagePath)),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(
+                      width: 331,
+                      height: 63,
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(30, 29, 29, 29),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text(
+                        'Cake Name',
+                        style: Theme.of(context).textTheme.titleLarge,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          SizedBox(height: 50),
+          Container(height: 100, width: 374, color: Colors.red),
+        ],
       ),
     );
   }
