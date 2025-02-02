f87aac2164b2f132b3834a889a19e26c0b93c1fc
Navyrine <wahidadinugrohoo@gmail.com>
2025-02-02 19:30:28 +0800

    Mengimplementasikan image classification

--- lib/provider/image_provider.dart
+++ lib/provider/image_provider.dart
@@ -1,13 +1,29 @@
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image/image.dart' as img;
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class ImageNotifier extends StateNotifier<File?> {
   ImageNotifier() : super(null);
 
   File? pickedImage;
+  Interpreter? interpreter;
+  List<String>? labels;
+
+  Future<void> loadModel() async {
+    interpreter = await Interpreter.fromAsset('assets/model.tflite');
+    labels = await loadLabels();
+  }
+
+  Future<List<String>> loadLabels() async {
+    final labelData = await rootBundle.loadString('assets/labels.txt');
+
+    return labelData.split('\n').map((e) => e.trim()).toList();
+  }
 
   Future<File?> pickImage(ImageSource source) async {
     File? tempImage;
@@ -19,10 +35,14 @@ class ImageNotifier extends StateNotifier<File?> {
 
       if (pickedImage == null) {
         return null;
-      } else {
-        tempImage = File(pickedImage.path);
-        tempImage = await cropImage(imageFile: tempImage);
-        state = tempImage;
+      }
+
+      tempImage = File(pickedImage.path);
+      tempImage = await cropImage(imageFile: tempImage);
+      state = tempImage;
+
+      if (tempImage != null) {
+        classifyImage(tempImage);
       }
     } catch (error) {
       throw Exception(error);
@@ -44,6 +64,64 @@ class ImageNotifier extends StateNotifier<File?> {
       }
     } catch (error) {
       throw Exception(error.toString());
+    }
+  }
+
+  Future<void> classifyImage(File image) async {
+    if (interpreter == null) {
+      await loadModel();
+    }
+
+    var imageBytes = image.readAsBytesSync();
+    img.Image? decodedImage = img.decodeImage(imageBytes);
+    img.Image resizedImage = img.copyResize(
+      decodedImage!,
+      width: 256,
+      height: 256,
+    );
+
+    var input = List.generate(
+      1,
+      (index) => List.generate(
+        256,
+        (i) => List.generate(256, (j) => List.generate(3, (j) => 0.0)),
+      ),
+    );
+
+    for (int y = 0; y < 256; y++) {
+      for (int x = 0; x < 256; x++) {
+        var pixel = resizedImage.getPixel(x, y);
+        input[0][y][x] = [
+          (pixel.r / 255.0),
+          (pixel.g / 255.0),
+          (pixel.b / 255.0),
+        ];
+      }
+    }
+
+    var output = List.filled(
+      1 * labels!.length,
+      0.0,
+    ).reshape([1, labels!.length]);
+
+    interpreter!.run(input, output);
+
+    List<Map<String, dynamic>> predictions = List.generate(
+      labels!.length,
+      (index) => {"label": labels![index], "confidence": output[0][index]},
+    );
+
+    predictions.sort(
+      (a, b) =>
+          (b["confidence"] as double).compareTo(a["confidence"] as double),
+    );
+
+    List<Map<String, dynamic>> topPredictions = predictions.take(3).toList();
+
+    for (var prediction in topPredictions) {
+      print(
+        "Label: ${prediction["label"]}, Confidence: ${(prediction["confidence"] as double) * 100}%",
+      );
     }
   }
 }
